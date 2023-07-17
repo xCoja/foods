77114f853995734cc44fadcaf85652f2fbfe557d
Coja <91964951+xCoja@users.noreply.github.com>
2023-07-17 14:55:12 +0200

    Add files via upload

--- test sajt/script.js
+++ test sajt/script.js
@@ -9,3 +9,33 @@ menu.addEventListener("click", function () {
 window.onscroll = () => {
     navbar.classList.remove("active");
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  setTimeout(showSlides, 2000); // Change image every 2 seconds 
+
+}
